7c90d8011f17a3254a25542938316035aa9b415c
khushmandeep10 <157265176+khushmandeep10@users.noreply.github.com>
2024-12-03 20:31:22 -0500

    Completes the assignment
    Completes the assignment
    
    Signed-off-by: khushmandeep10 <157265176+khushmandeep10@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
+const errorBox = document.getElementById("msg");
 const store_corrected = document.getElementById("store_correct");
 const store_incorrected = document.getElementById("store_incorrect");
+const question_d = document.getElementById("question");
+const start_button = document.querySelector(".start_button");
+const question_container = document.querySelector(".question-container");
+const register_container = document.querySelector(".register-container");
+let answer_o1_d = document.getElementById("option1");
+let answer_o2_d = document.getElementById("option2");
+let answer_o3_d = document.getElementById("option3");
+let answer_o4_d = document.getElementById("option4");
+let quiz_diff = "easy";
+
 store_corrected.innerText = localStorage.getItem("correct") ? localStorage.getItem("correct") : 0;
 store_incorrected.innerText = localStorage.getItem("incorrect") ? localStorage.getItem("incorrect") : 0;
 
-var quiz_diff = "easy";
+
+
 function onStartClick() {
-    var selectElement = document.getElementById("difficulty");
+    let selectElement = document.getElementById("difficulty");
     quiz_diff = selectElement.value;
 }
 
 function onResetScoreClick() {
+    store_corrected.innerText = 0;
+    store_incorrected.innerText = 0;
     localStorage.setItem("correct", 0);
     localStorage.setItem("incorrect", 0);
-    location.reload();
 }
-
-const start_button = document.querySelector(".start_button");
-const question_container = document.querySelector(".question-container");
-const register_container = document.querySelector(".register-container");
 
 const callAPI = () => {
     const api_url = `https://quizapi.io/api/v1/questions?apiKey=rCGVCEDqJJd8TXsunCOyAilovahAzmbg6yGp4XQT&limit=10&difficulty=${quiz_diff}`;
@@ -27,16 +36,11 @@ const callAPI = () => {
 
 start_button.addEventListener("click", callAPI);
 
-var index = 0;
-var api_data;
-var total = 0;
-var same_question;
-var answer_o1_d;
-var answer_o2_d;
-var answer_o3_d;
-var answer_o4_d;
-var time;
-var interval;
+let index = 0;
+let api_data;
+let total = 0;
+let time;
+let interval;
 
 async function getdata(url) {
     const response = await fetch(url);
@@ -77,19 +81,20 @@ const onNextquestion = () => {
     }
 
     index++;
-    same_question = false;
 
-    var timer = document.getElementById("timer");
+    let timer = document.getElementById("timer");
     setInterval(() => {
         timer.innerText = time;
     }, 1000);
 
     if (api_data) {
-        const question_d = document.getElementById("question");
-        answer_o1_d = document.getElementById("option1");
-        answer_o2_d = document.getElementById("option2");
-        answer_o3_d = document.getElementById("option3");
-        answer_o4_d = document.getElementById("option4");
+        errorBox.innerText = "";
+        const allAnswers = [answer_o1_d, answer_o2_d, answer_o3_d, answer_o4_d];
+        allAnswers.forEach((option) => {
+            if (option) {
+                option.disabled = false;
+            }
+        });
         const qnumber = document.getElementById("qnumber");
         const correct_a = document.getElementById("correctanswer");
         const incorrect_a = document.getElementById("incorrectanswer");
@@ -99,84 +104,55 @@ const onNextquestion = () => {
         correct_a.innerText = total;
         incorrect_a.innerText = (index - 1) - total;
 
+        const options = [
+            { element: answer_o1_d, text: api_data[index - 1].answers.answer_a },
+            { element: answer_o2_d, text: api_data[index - 1].answers.answer_b },
+            { element: answer_o3_d, text: api_data[index - 1].answers.answer_c },
+            { element: answer_o4_d, text: api_data[index - 1].answers.answer_d },
+        ];
 
-        if (api_data[index - 1].answers.answer_a !== null) {
-            answer_o1_d.classList.remove("remove")
-            answer_o1_d.innerText = api_data[index - 1].answers.answer_a;
-            answer_o1_d.classList.add("option-1");
-            answer_o1_d.onclick = () => {
-                onScoreCalculator("1", answer_o1_d);
-            };
-        } else {
-            answer_o1_d.classList.add("remove")
-        }
-
-        if (api_data[index - 1].answers.answer_b !== null) {
-            answer_o2_d.classList.remove("remove")
-            answer_o2_d.innerText = api_data[index - 1].answers.answer_b;
-            answer_o2_d.classList.add("option-2");
-            answer_o2_d.onclick = () => {
-                onScoreCalculator("2", answer_o2_d);
-            };
-        } else {
-            answer_o2_d.classList.add("remove")
-        }
-
-        if (api_data[index - 1].answers.answer_c !== null) {
-            answer_o3_d.classList.remove("remove")
-            answer_o3_d.innerText = api_data[index - 1].answers.answer_c;
-            answer_o3_d.classList.add("option-3");
-            answer_o3_d.onclick = () => {
-                onScoreCalculator("3", answer_o3_d);
-            };
-        } else {
-            answer_o3_d.classList.add("remove")
-        }
-
-        if (api_data[index - 1].answers.answer_d !== null) {
-            answer_o4_d.classList.remove("remove")
-            answer_o4_d.innerText = api_data[index - 1].answers.answer_d;
-            answer_o4_d.classList.add("option-4");
-            answer_o4_d.onclick = () => {
-                onScoreCalculator("4", answer_o4_d);
-            };
-        } else {
-            answer_o4_d.classList.add("remove")
-        }
+        options.forEach((opt, idx) => {
+            if (opt.text !== null) {
+                opt.element.classList.remove("remove");
+                opt.element.innerText = opt.text;
+                opt.element.onclick = () => onScoreCalculator(idx + 1, opt.element);
+            } else {
+                opt.element.classList.add("remove");
+            }
+        });
 
         const nextq_btn = document.getElementById("s-answer");
         nextq_btn.classList.add("nextq-btn");
-        if (index <= 9) {
-            nextq_btn.innerText = "Next Question";
-        } else {
-            nextq_btn.innerText = "Submit";
-        }
+        nextq_btn.innerText = index <= 9 ? "Next Question" : "Submit";
         nextq_btn.onclick = onNextquestion;
     }
 };
 
 const onScoreCalculator = (user_choice, button) => {
-    if (answer_o1_d && answer_o1_d.classList.contains("choosen")) {
-        answer_o1_d.classList.remove("choosen");
-    } else if (answer_o2_d && answer_o2_d.classList.contains("choosen")) {
-        answer_o2_d.classList.remove("choosen");
-    } else if (answer_o3_d && answer_o3_d.classList.contains("choosen")) {
-        answer_o3_d.classList.remove("choosen");
-    } else if (answer_o4_d && answer_o4_d.classList.contains("choosen")) {
-        answer_o4_d.classList.remove("choosen");
-    }
-
+    const allAnswers = [answer_o1_d, answer_o2_d, answer_o3_d, answer_o4_d];
+    allAnswers.forEach(answer => {
+        if (answer && answer.classList.contains("choosen")) {
+            answer.classList.remove("choosen");
+        }
+    });
     button.classList.add("choosen");
+    const isAnswer = Object.values(api_data[index - 1].correct_answers)[user_choice - 1];
 
-    const isAnswer = Object.values(api_data[index - 1].correct_answers)[
-        user_choice - 1
-    ];
-    if (isAnswer == "true" && !same_question) {
+    allAnswers.forEach((ans) => {
+        if (ans) {
+            ans.disabled = true;
+        }
+    });
+
+    if (isAnswer === "true") {
         total++;
-        same_question = true;
-    }
-    if (isAnswer == "false" && same_question) {
-        total--;
-        same_question = null;
+        errorBox.innerText = "😀 Correct Answer!";
+        errorBox.classList.add("text-success", "suc-bg");
+        errorBox.classList.remove("text-danger", "fail-bg");
+    } else {
+        errorBox.innerText = "😢 Incorrect Answer!";
+        errorBox.classList.add("text-danger", "fail-bg");
+        errorBox.classList.remove("text-success", "suc-bg");
+
     }
 };
